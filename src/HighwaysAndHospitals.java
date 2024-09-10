557ce69f4106496380ddc971535668eb08b7624b
soccer47 <stevie.halprin@menloschool.org>
2024-09-09 18:36:54 -0700

    second commit

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Highways & Hospitals
@@ -38,6 +40,9 @@ public class HighwaysAndHospitals {
         // Each city's  connections to be listed at roads[city]
         ArrayList<Integer>[] roads = new ArrayList[n + 1];
 
+        // Boolean array that holds each city's hospital status (if city has hospital, hospitals[city] == true)
+        boolean[] hospitals = new boolean[n + 1];
+
         // For each possible city connection listed in cities[][], add the 2nd node listed at the index
         // to the list of possible connections at connections[1st city], and vice versa
         for (int i = 0; i < cities.length; i++) {
@@ -45,14 +50,50 @@ public class HighwaysAndHospitals {
             connections[cities[i][1]].add(cities[i][0]);
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             // If the given node cannot build any highways to other city, add a hospital to the city
-            if (connections[i + 1].size() == 0) {
+            if (connections[i].size() == 0) {
                 cost += hospitalCost;
+                hospitals[i] = true;
             }
-            // Otherwise look through possible cities to connect to
+            // Otherwise look through possible routes that can be created through other cities to hospitals
             else {
-                for (int j = 0; j < roads[i + 1].size(); j++) {
+                // Creates new Queue to which the cities to be checked are added
+                Queue<Integer> toVisit = new LinkedList<>();
+                // Creates new array where the previously visited city of each city is stored at lastCity[city number]
+                int[] lastCity = new int[n + 1];
+                // City number of closest city with hospital initialized to 0
+                int validCity = 0;
+                // City number of city currently being checked initialized to i
+                int current = i;
+
+                // While a city with a hospital hasn't been found, continue looping
+                while (validCity == 0) {
+                    // If there aren't any more cities to visit in the queue, build a hospital at
+                    // the current city (there aren't any already-created hospitals to connect to)
+                    if (toVisit.peek() == null) {
+                        return -1;
+                    }
+
+                    // Current square is the next value in the toVisit queue
+                    current = toVisit.remove();
+
+                    // If city has a hospital, set validCity to the city's number and break
+                    if (hospitals[current] == true) {
+                        validCity = current;
+                        break;
+                    }
+
+                    // Add the next possible cities to the toVisit queue (if they aren't already visited)
+                    for (city : connections[current]) {
+                        // Makes sure that the given city hasn't been visited yet
+                        if (lastCity[city] == 0) {
+                            // Add the result square to the toVisit queue
+                            toVisit.add(resultSq);
+                            // Set the previous city of the next city equal to the current city
+                            lastCity[city] = current;
+                        }
+                    }
 
                 }
             }
@@ -63,5 +104,6 @@ public class HighwaysAndHospitals {
     }
 
 
-    public static int connectedHospitals(int city, )
+    // Method that finds closest city (city fewest connections apart) with a hospital
+    public static int connectedHospitals(int city, boolean[] hospitals)
 }
